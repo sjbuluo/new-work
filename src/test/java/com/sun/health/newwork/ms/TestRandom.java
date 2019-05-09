61ad8c951b8b5c81903bfab5687902c84f989152
孙健 <sunjian6@bonc.com.cn>
2019-05-09 11:52:30 +0800

    美摄项目技术学习

--- src/test/java/com/sun/health/newwork/ms/TestRandom.java
+++ src/test/java/com/sun/health/newwork/ms/TestRandom.java
@@ -1,7 +1,31 @@
 package com.sun.health.newwork.ms;
 
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Random;
+
 /**
  * Created by 华硕 on 2019-05-06.
  */
 public class TestRandom {
+
+    @Test
+    public void test1() {
+        HashSet<Integer> set = new HashSet<>();
+        Random random = new Random();
+        while(set.size() < 4) {
+            set.add(random.nextInt(4));
+        }
+        System.out.println(set);
+        Integer i1 = new Integer(1);
+        System.out.println(i1.hashCode());
+        Integer i2 = new Integer(2);
+        System.out.println(i2.hashCode());
+        Integer i3 = new Integer(3);
+        System.out.println(i3.hashCode());
+        Integer i4 = new Integer(4);
+        System.out.println(i4.hashCode());
+    }
+
 }
--- src/test/java/com/sun/health/newwork/ms/file/SimpleFileTest.java
+++ src/test/java/com/sun/health/newwork/ms/file/SimpleFileTest.java
@@ -1,7 +1,28 @@
 package com.sun.health.newwork.ms.file;
 
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+
 /**
  * Created by 华硕 on 2019-05-05.
  */
-public class SimpleFileTest {
+public class SimpleFileTest
+{
+    @Test
+    public void test1() throws IOException {
+        File file = new File("");
+        Files.copy(file.toPath(), file.toPath());
+    }
+
+    @Test
+    public void test2() throws IOException {
+        File file = new File("");
+        FileChannel inputChannel = new FileInputStream("").getChannel();
+        FileChannel outputChannel = new FileOutputStream("").getChannel();
+        outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+
+    }
 }
